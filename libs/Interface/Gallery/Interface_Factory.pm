d43e754aed385a28d3bf969b9cd282b7f17df606
Alexey Kosarev <alex@homyaki.info>
2012-05-20 14:39:17 +0400

    Move interface handlers to DB Final step

--- libs/Interface/Gallery/Interface_Factory.pm
+++ libs/Interface/Gallery/Interface_Factory.pm
@@ -7,6 +7,6 @@ use Exporter;
 use base 'Homyaki::Interface::Interface_Factory';
 
 use constant DEFAULT_FORM  => 'default';
-use constant NTERFACE_NAME => 'gallery';
+use constant INTERFACE_NAME => 'gallery';
 
 1;
