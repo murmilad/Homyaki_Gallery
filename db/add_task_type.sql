3a9e725f4055bd41b323265a3e96818ecf2f70bd
Alexey Kosarev <alex@homyaki.info>
2012-05-30 22:00:15 +0400

    Task types script. DB using

--- db/add_task_type.sql
+++ db/add_task_type.sql
@@ -1,2 +1,4 @@
+use tasks;
 INSERT INTO `task_types` (handler, name)
-	VALUES ('Homyaki::Task_Manager::Task::Gallery_Update','Akosarev Gallery update');
+	VALUES ('Homyaki::Task_Manager::Task::Build_Gallery','Akosarev Gallery build'),
+	('Homyaki::Task_Manager::Task::Gallery_Update','Akosarev Gallery update');
