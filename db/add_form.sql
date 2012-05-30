868d805dbb2833518e853a94853fd029e46f84c0
Alexey Kosarev <alex@homyaki.info>
2012-05-30 22:15:55 +0400

    Add gallery update tasks interfaces handlers

--- db/add_form.sql
+++ db/add_form.sql
@@ -4,4 +4,7 @@ INSERT INTO `form_handlers` (handler, name, description, interface_name)
 		('Homyaki::Interface::Gallery::News' , 'news', 'News about blog changes form handler', 'gallery')
 		,('Homyaki::Interface::Gallery::Auth', 'auth', 'Auth to gallery form handler'        , 'gallery')
 		,('Homyaki::Interface::Gallery'      , 'base', 'Base gallery form handler'           , 'gallery')
-		,('Homyaki::Interface::Gallery::Data', 'data', 'Image info form handler'             , 'gallery');
+		,('Homyaki::Interface::Gallery::Data', 'data', 'Image info form handler'             , 'gallery')
+
+		,('Homyaki::Interface::Task_Manager::Gallery_Update_Task', 'gallery_update' , 'Gallery update form handler', 'task')
+		,('Homyaki::Interface::Task_Manager::Build_Gallery', 'build_gallery' , 'Build gallery form handler', 'task');
