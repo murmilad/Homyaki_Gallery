c1f24bbe5a339bf09ba23c886d8def8a377da783
Alexey Kosarev <alex@homyaki.info>
2012-06-03 20:33:44 +0400

    Create HTML Albums builder plugin

--- libs/Task_Manager/Task/Build_Gallery/Interface_Builder/Factory.pm
+++ libs/Task_Manager/Task/Build_Gallery/Interface_Builder/Factory.pm
@@ -7,9 +7,9 @@ use Exporter;
 use base 'Homyaki::Factory';
 
 use constant INTERFACE_BUILDER_MAP => {
-	default                => 'Homyaki::Task_Manager::Task::Build_Gallery::Interface_Builder::DF_Gallery',
-	df_gallery             => 'Homyaki::Task_Manager::Task::Build_Gallery::Interface_Builder::DF_Gallery',
-	df_gallery_html_albums => 'Homyaki::Task_Manager::Task::Build_Gallery::Interface_Builder::DF_Gallery_HTML_Albums',
+	default     => 'Homyaki::Task_Manager::Task::Build_Gallery::Interface_Builder::DF_Gallery',
+	df_gallery  => 'Homyaki::Task_Manager::Task::Build_Gallery::Interface_Builder::DF_Gallery',
+	html_albums => 'Homyaki::Task_Manager::Task::Build_Gallery::Interface_Builder::HTML_Albums',
 };
 
 sub create_builder {
