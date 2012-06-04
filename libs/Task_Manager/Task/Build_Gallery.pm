87890fce1f433e3da44d59db9fabfd53a7de2b70
Alexey Kosarev <alex@homyaki.info>
2012-06-04 18:58:28 +0400

    Release add images path to builder fix12

--- libs/Task_Manager/Task/Build_Gallery.pm
+++ libs/Task_Manager/Task/Build_Gallery.pm
@@ -810,6 +810,7 @@ sub start {
 			params     => {
 				update       => 1,
 				gallery_path => &GALLERY_PATH,
+				images_path  => &PIC_PATH,
 				xml_path     => &XML_PATH,
 				web_path     => &SITE_URL,
 				web_login    => &SITE_LOGIN,
